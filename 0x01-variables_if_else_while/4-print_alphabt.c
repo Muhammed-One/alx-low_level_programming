f98e42f4361dab6e9cb361d271e2ce1e8f4ca649
Muhammed-One <midogeme86@gmail.com>
2023-08-12 04:29:47 +0300

    hamo

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Description: A C program that prints alphabet with putchar function & expect e,q
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch == 'q' || ch == 'e')
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
