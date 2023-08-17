84e5d2e84d751710a6b1f7cb34fff76be074c5cf
Muhammed-One <midogeme86@gmail.com>
2023-08-17 21:53:35 +0300

    hamo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 int
+ * @AYOO: first int
+ * @HAMO: second int
+ * Return: addition of the multiplicattion
+ */
+
+int mul(int AYOO, int HAMO)
+{
+return (AYOO * HAMO);
+}
