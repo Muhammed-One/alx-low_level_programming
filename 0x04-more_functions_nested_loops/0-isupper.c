d0c1d7c51a58dcf7ad0340c00f63b72c7a60c3c7
Muhammed-One <midogeme86@gmail.com>
2023-08-17 20:42:35 +0300

    hamo

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - uppercase letters
- * @HAMO: char to check
+ * @c: char to check
  *
  * Return: 0 or 1
  */
 
-int _isupper(int HAMO)
+int _isupper(int c)
 {
-if (HAMO >= 'A' && HAMO <= 'Z')
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
